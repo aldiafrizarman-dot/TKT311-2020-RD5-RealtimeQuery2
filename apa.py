189bb7d9b03a17492a86a4b773bdec421c59b86f
rahimi <74383451+rahimi-tekkom@users.noreply.github.com>
2020-12-21 09:06:11 +0700

    Add files via upload

---
+++ apa.py
@@ -0,0 +1,50 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.uix.textinput import TextInput
+from kivy.core.window import Window
+from kivymd.theming import ThemeManager
+
+Window.size = (450, 650)
+
+LoginPage = '''
+MDFloatLayout:
+    MDLabel:
+        text: "RRRRRAAHIIMIII"
+        pos_hint: {"center_x": .5, "center_y": .60}
+        size_hint_x: .7
+        font_style: "H5"
+        halign: "center"
+        theme_text_color: "Custom"
+        text_color: 1, 0, 0, 1
+
+    MDLabel:
+        text: "nnnnoooongggbiiuttt"
+        pos_hint: {"center_y": .6}
+        font_style: "Body1"
+        halign: "center"
+        theme_text_color: "Custom"
+        text_color: 1, 0, 0, 1
+        password: True
+
+    MDFloatingActionButton:
+        icon: "plus"
+        pos_hint: {"center_x": .3, "center_y": .6}
+        md_bg_color: app.theme_cls.primary_color
+
+
+
+
+'''
+class RQApp(MDApp):
+    def build(self):
+        self.theme_cls.primary_palette = "Cyan"
+        login_page = Builder.load_string(LoginPage)
+        return login_page
+
+if __name__ == '__apa__':
+    RQApp().run()
---
+++ tampil.py
@@ -0,0 +1,24 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.uix.textinput import TextInput
+from kivy.core.window import Window
+from kivymd.theming import ThemeManager
+
+Window.size = (450, 650)
+
+LoginPage = '''
+
+
+'''
+
+
+
+
+
+if __name__ == "__main__":
+    RQApp().run()
